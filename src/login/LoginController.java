d0bbbcd4676333590ab87fc9808c9cb2e39962c9
hlebs <glebszypula1997@gmail.com>
2021-01-01 22:08:17 +0100

    Fisrt commit

---
+++ src/login/LoginController.java
@@ -0,0 +1,21 @@
+package login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private void Action(ActionEvent event) {
+        System.out.println("a");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
