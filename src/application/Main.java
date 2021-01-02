108f0803a3eb078d50b1d7e1a3abd55b88db61b0
hlebs <glebszypula1997@gmail.com>
2021-01-02 11:49:09 +0100

    Login window

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,14 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        URL url = new File("C:\\Users\\hlebs\\Desktop\\SEMESTR 5\\BD1\\Project_v1\\Application\\src\\application.login\\Login.fxml").toURI().toURL();
-//        Parent root = FXMLLoader.load(url);
         Parent root = FXMLLoader.load(getClass().getResource("login/Login.fxml"));
         primaryStage.setTitle("Hleb Shypula - Projekt - Bazy Danych 1");
         primaryStage.setScene(new Scene(root, 1200, 750));
         primaryStage.show();
-
-
     }
 
     public static void main(String[] args) {
